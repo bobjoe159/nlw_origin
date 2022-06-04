1fc0fff8abb0273fdc0baabd09c844488044ade9
bobjoe159 <pedroh1001@hotmail.com>
2022-06-04 01:14:50 -0300

    Criado seção about e contact

--- main.js
+++ main.js
@@ -12,4 +12,17 @@ function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
 
-ScrollReveal().reveal('#home')
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 1200
+}).reveal(`
+#home, 
+#home img, 
+#home .stats, 
+#services,
+#services header,
+#services .card
+#about,
+#about header,
+#about .content`)
